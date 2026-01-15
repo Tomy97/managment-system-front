8570cff73a6b8f0fee6fff68afa62917afa350ff
Tomas Pandullo <64372041+Tomy97@users.noreply.github.com>
2026-01-15 10:43:51 -0300

    fix: remove base path from Vite configuration to streamline deployment setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/managment-system-front/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
